a6c9f38bac6b65e6aec570aef6f05fa941dc1f73
Jesse Barajas <jb19932010@yahoo.com>
2019-05-15 23:49:21 -0500

    Changed main code
    Took out extra buttons and static displays from tutorial, should now be just a windo

--- Flute_Learning_Program/main.cpp
+++ Flute_Learning_Program/main.cpp
@@ -11,13 +11,7 @@
 
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
-void AddMenus(HWND);
 void AddControls(HWND);
-void loadImages();
-
-HWND hName, hAge, hOut, hLogo, hGenerate;
-HMENU hMenu;
-HBITMAP hLogoImage, hGenerateImage;
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR args, int ncmdshow)
 {
@@ -32,7 +26,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR args, int ncmdsh
     if(!RegisterClassW(&wc))
         return -1;
 
-    CreateWindowW(L"myWindowClass", L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500,
+    CreateWindowW(L"myWindowClass", L"Flute Learning Program", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 720, 480,
                   NULL, NULL, NULL, NULL);
     MSG msg = {0};
 
@@ -56,31 +50,9 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
         case FILE_MENU_EXIT:
             DestroyWindow(hWnd);
             break;
-        case FILE_MENU_NEW:
-            MessageBeep(MB_ICONINFORMATION);
-            break;
-        case GENERATE_BUTTON:
-
-            char name[30], age[10], out[50];
-
-            GetWindowText(hName, name, 30);
-            GetWindowText(hAge, age, 10);
-
-            strcpy(out, name);
-            strcat(out," is ");
-            strcat(out, age);
-            strcat(out," years old.");
-
-            SetWindowText(hOut, out);
-
-            break;
         }
-
-
         break;
     case WM_CREATE:
-        loadImages();
-        AddMenus(hWnd);
         AddControls(hWnd);
         break;
     case WM_DESTROY:
@@ -91,44 +63,8 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
     }
 }
 
-void AddMenus(HWND hWnd)
-{
-    hMenu = CreateMenu();
-    HMENU hFileMenu = CreateMenu();
-    HMENU hSubMenu = CreateMenu();
-
-    AppendMenu(hSubMenu, MF_STRING, NULL, "SubMenu Item");
-
-    AppendMenu(hFileMenu, MF_STRING, FILE_MENU_NEW, "New");
-    AppendMenu(hFileMenu, MF_POPUP, (UINT_PTR)hSubMenu, "Open SubMenu");
-    AppendMenu(hFileMenu, MF_SEPARATOR, NULL, NULL);
-    AppendMenu(hFileMenu, MF_STRING, FILE_MENU_EXIT, "Quit");
-
-    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu,  "File");
-    AppendMenu(hMenu, MF_STRING, NULL,  "Help");
-
-    SetMenu(hWnd, hMenu);
-}
-
 void AddControls(HWND hWnd)
 {
-    CreateWindowW(L"static", L"Name :", WS_VISIBLE | WS_CHILD, 100, 50, 98, 38, hWnd, NULL, NULL, NULL);
-    hName = CreateWindowW(L"edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 200, 50, 98, 38, hWnd, NULL, NULL, NULL);
-
-    CreateWindowW(L"static", L"Age :", WS_VISIBLE | WS_CHILD, 100, 90, 98, 38, hWnd, NULL, NULL, NULL);
-    hAge = CreateWindowW(L"edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 200, 90, 98, 38, hWnd, NULL, NULL, NULL);
-
-    HWND hBut = CreateWindowW(L"Button", NULL, WS_VISIBLE | WS_CHILD | BS_BITMAP, 150, 140, 98, 38, hWnd, (HMENU)GENERATE_BUTTON, NULL, NULL);
-    SendMessageW(hBut, BM_SETIMAGE, IMAGE_BITMAP,(LPARAM)hGenerateImage);
-
-    hOut = CreateWindowW(L"edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 100, 200, 300, 200, hWnd, NULL, NULL, NULL);
-    hLogo = CreateWindowW(L"static", NULL, WS_VISIBLE | WS_CHILD | SS_BITMAP, 350, 60, 100, 100, hWnd, NULL, NULL, NULL);
-    SendMessageW(hLogo, STM_SETIMAGE, IMAGE_BITMAP,(LPARAM) hLogoImage);
-}
-
-void loadImages()
-{
-    hLogoImage = (HBITMAP)LoadImageW(NULL, L"Logo.bmp", IMAGE_BITMAP, 100, 100, LR_LOADFROMFILE);
-    hGenerateImage = (HBITMAP)LoadImageW(NULL, L"Generate.bmp", IMAGE_BITMAP, 125, 58, LR_LOADFROMFILE);
+    ;
 }
 
