3ee4db14d919174dd4db21d6f8fd25b34d3b602f
Jesse Barajas <jb19932010@yahoo.com>
2019-05-18 23:38:11 -0500

    added new functions
    removed tutorial functions and added new custom ones to represent new buttons.

--- Flute_Learning_Program/main.cpp
+++ Flute_Learning_Program/main.cpp
@@ -4,11 +4,28 @@
 #define FILE_MENU_NEW 1
 #define FILE_MENU_OPEN 2
 #define FILE_MENU_EXIT 3
-#define GENERATE_BUTTON 4
+#define FREEPLAY_BUTTON 4
+#define CHALLENGE_BUTTON 5
+#define OPTION_BUTTON 6
 
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
-void AddControls(HWND);
+void MainMenu_creation(HWND);
+void MainMenu_deletion(HWND);
+void Option_creation(HWND);
+
+HWND hFree, hChallenge, hOption;
+
+//variables to get center of window
+
+int windowlength_x = 720;
+int windowlength_y = 480;
+
+int buttonlength_x = 100;
+int buttonlength_y = 50;
+
+int center_x = (windowlength_x / 2) - (buttonlength_x / 2);
+int center_y = (windowlength_y / 2) - (buttonlength_y / 2);
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR args, int ncmdshow)
 {
@@ -23,7 +40,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR args, int ncmdsh
     if(!RegisterClassW(&wc))
         return -1;
 
-    CreateWindowW(L"myWindowClass", L"Flute Learning Program", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 720, 480,
+    CreateWindowW(L"myWindowClass", L"Flute Learning Program", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, windowlength_x, windowlength_y,
                   NULL, NULL, NULL, NULL);
     MSG msg = {0};
 
@@ -47,10 +64,16 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
         case FILE_MENU_EXIT:
             DestroyWindow(hWnd);
             break;
+        case FREEPLAY_BUTTON:
+            break;
+        case CHALLENGE_BUTTON:
+            break;
+        case OPTION_BUTTON:
+            break;
         }
         break;
     case WM_CREATE:
-        AddControls(hWnd);
+        MainMenu_creation(hWnd);
         break;
     case WM_DESTROY:
         PostQuitMessage(0);
@@ -60,10 +83,21 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
     }
 }
 
-void AddControls(HWND hWnd)
+void MainMenu_creation(HWND hWnd)
+{
+    HWND hFree = CreateWindowW(L"Button", L"Free Play", WS_VISIBLE | WS_CHILD, center_x, 140, buttonlength_x, buttonlength_y, hWnd, (HMENU)FREEPLAY_BUTTON, NULL, NULL);
+
+    HWND hChallenge = CreateWindowW(L"Button", L"Challenges", WS_VISIBLE | WS_CHILD, center_x, 200, buttonlength_x, buttonlength_y, hWnd, (HMENU)CHALLENGE_BUTTON, NULL, NULL);
+
+    HWND hOption = CreateWindowW(L"Button", L"Options", WS_VISIBLE | WS_CHILD, center_x, 260, buttonlength_x, buttonlength_y, hWnd, (HMENU)OPTION_BUTTON, NULL, NULL);
+}
+
+void MainMenu_deletion(HWND hWnd)
 {
     ;
 }
 
-//THIS IS THE TITS JESSE
-//Yeah it is
+void Option_creation(HWND hWnd)
+{
+    ;
+}
