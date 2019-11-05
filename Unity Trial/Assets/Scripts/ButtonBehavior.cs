3a3b8580efad8d2ebf0c3c56d0c3adcc71823b15
Jesse Barajas <jb19932010@yahoo.com>
2019-11-05 11:20:53 -0600

    Add Button Functionality
    Able to move between scenes with UI buttons

---
+++ Unity Trial/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public void MainMenuButton()
+    {
+        Debug.Log("Main Menu Button");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void FreePlayButton()
+    {
+        Debug.Log("FreePlay Button");
+        SceneManager.LoadScene("FreePlay");
+    }
+
+    public void ChallengeButton()
+    {
+        Debug.Log("Challenge Button");
+        SceneManager.LoadScene("Challenge");
+    }
+
+    public void OptionsButton()
+    {
+        Debug.Log("Options Button");
+        SceneManager.LoadScene("Options");
+    }
+}
+
+
